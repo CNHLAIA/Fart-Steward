1cb8ef706db7cf6cffa59449feb6372a9b884442
HLAIA <813825686@qq.com>
2026-02-16 11:38:45 +0800

    feat(i18n): translate all frontend components

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,16 +1,19 @@
 require("@testing-library/jest-dom");
 
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({
-    t: (key) => key,
-    i18n: {
-      changeLanguage: jest.fn().mockResolvedValue(t => t),
-      language: 'en',
+jest.mock('react-i18next', () => {
+  const t = (key) => key;
+  return {
+    useTranslation: () => ({
+      t,
+      i18n: {
+        changeLanguage: jest.fn().mockResolvedValue(t),
+        language: 'en',
+      },
+    }),
+    initReactI18next: {
+      type: '3rdParty',
+      init: jest.fn(),
     },
-  }),
-  initReactI18next: {
-    type: '3rdParty',
-    init: jest.fn(),
-  },
-}));
+  };
+});
 
